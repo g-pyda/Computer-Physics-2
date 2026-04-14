59179ef94cb9b7a60afdc035f15bbfa6a13c7ee7
g-pyda <gabriela.pyda4@gmail.com>
2026-04-14 10:16:57 +0200

    lab2 on classes

---
+++ lab2/engine.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <vector>
+// 126 from lecture
+
+void process_node(
+    std::vector<std::vector<double>> x,
+    std::vector<std::vector<double>> y,
+    int node_id,
+    int N,
+    double L,
+    bool biparabolic
+);
